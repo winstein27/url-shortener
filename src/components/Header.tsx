2af50b3952cb6f21fe7f585361a7c38a4fed3c65
Winstein Martins <sriwinstein@gmail.com>
2023-06-05 17:46:07 -0300

    :sparkles: mobile header
    Adding mobile menu to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import { AiOutlineMenu } from "react-icons/ai";
 
 import styles from "./Header.module.scss";
 
+const menu = (
+  <nav className={styles.menu}>
+    <ul>
+      <div>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Resources</li>
+      </div>
+      <div className={styles.action}>
+        <li>Login</li>
+        <li className={styles.highlighted}>Sign Up</li>
+      </div>
+    </ul>
+  </nav>
+);
+
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuClickHandler = () => setIsMenuOpen((prevMenu) => !prevMenu);
+
   return (
     <header className={styles.container}>
       <h1>Shortly</h1>
-      <AiOutlineMenu />
+      <button onClick={menuClickHandler}>
+        <AiOutlineMenu />
+      </button>
+      {isMenuOpen &&
+        createPortal(menu, document.getElementById("overlay") as HTMLElement)}
     </header>
   );
 };

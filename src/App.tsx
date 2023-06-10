3bac9cab7c9da162667fbb178aa66bdceb133aef
Winstein Martins <sriwinstein@gmail.com>
2023-06-09 23:57:55 -0300

    :sparkles: feat: start main
    Starting main section of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;

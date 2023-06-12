328411e53a8cd3b17be051f935933c5259dffcc8
Winstein Martins <sriwinstein@gmail.com>
2023-06-11 21:59:50 -0300

    :sparkles: feat: main content
    Adding image, slogan and some text

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,18 @@
+import Working from "../assets/images/illustration-working.svg";
+
 import styles from "./Main.module.scss";
 
 const Main = () => {
-  return <main></main>;
+  return (
+    <main className={styles.container}>
+      <img src={Working} alt="" className={styles["main-img"]} />
+      <h1 className={styles.slogan}>More than just shorter links</h1>
+      <p className={styles.detail}>
+        Build your brand's recognition and get detailed insights on how your
+        links are performing.
+      </p>
+    </main>
+  );
 };
 
 export default Main;

6b8fe6b90eb2fad70f5d5d3942a6ada78acfe860
Winstein Martins <sriwinstein@gmail.com>
2023-06-16 14:09:45 -0300

    :sparkles: feat: mobile footer
    Adding footer to mobile devices

---
+++ src/components/Footer.tsx
@@ -0,0 +1,72 @@
+import styles from "./Footer.module.scss";
+
+import Logo from "../assets/images/Logo";
+
+import Facebook from "../assets/images/icon-facebook.svg";
+import Twitter from "../assets/images/icon-twitter.svg";
+import Pinterest from "../assets/images/icon-pinterest.svg";
+import Instagram from "../assets/images/icon-instagram.svg";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Logo />
+      <ul>
+        <li>
+          Features
+          <ul className={styles.links}>
+            <li>
+              <a href="#">Link Shortening</a>
+            </li>
+            <li>
+              <a href="#">Branded Links</a>
+            </li>
+            <li>
+              <a href="#">Analytics</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          Resources
+          <ul className={styles.links}>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Developers</a>
+            </li>
+            <li>
+              <a href="#">Support</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          Company
+          <ul className={styles.links}>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Our Team</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <div className={styles.social}>
+        <img src={Facebook} alt="Facebook" />
+        <img src={Twitter} alt="Twitter" />
+        <img src={Pinterest} alt="Pinterest" />
+        <img src={Instagram} alt="Instagram" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

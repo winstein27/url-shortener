af32aa28d9320596de571ddf06d703616a20b231
Winstein Martins <sriwinstein@gmail.com>
2023-06-18 21:44:50 -0300

    :sparkles: feat: start boost
    Stargin boost section to mobile devices

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,21 +6,32 @@ import ShortenForm from "./main/ShortenForm";
 
 const Main = () => {
   return (
-    <main className={styles.container}>
-      <div className={styles.description}>
-        <img src={Working} alt="" className={styles["main-img"]} />
-        <div>
-          <h1 className={styles.slogan}>More than just shorter links</h1>
-          <p className={styles.detail}>
-            Build your brand's recognition and get detailed insights on how your
-            links are performing.
-          </p>
-          <a href="#">Get Started</a>
+    <>
+      <main className={styles.container}>
+        <div className={styles.description}>
+          <img src={Working} alt="" className={styles["main-img"]} />
+          <div>
+            <h1 className={styles.slogan}>More than just shorter links</h1>
+            <p className={styles.detail}>
+              Build your brand's recognition and get detailed insights on how
+              your links are performing.
+            </p>
+            <a href="#" className={styles.button}>
+              Get Started
+            </a>
+          </div>
         </div>
-      </div>
 
-      <ShortenForm />
-    </main>
+        <ShortenForm />
+      </main>
+
+      <div className={styles.boost}>
+        <h2>Boost your links today</h2>
+        <a href="#" className={styles.button}>
+          Get Started
+        </a>
+      </div>
+    </>
   );
 };
 

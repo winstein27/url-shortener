d9d5eaa1ecc830d6f793ffe49f5af31c4a54b46b
Winstein Martins <sriwinstein@gmail.com>
2023-06-23 22:13:49 -0300

    :construction: feat: features section

--- src/components/main/Features.tsx
+++ src/components/main/Features.tsx
@@ -3,7 +3,7 @@ import styles from "./Features.module.scss";
 const Features = () => {
   return (
     <div className={styles.features}>
-      <div>
+      <div className={styles["main-feature"]}>
         <h2>Advanced Statistics</h2>
         <p>
           Track how your links are performing across the web with our advanced
@@ -11,21 +11,21 @@ const Features = () => {
         </p>
       </div>
       <div>
-        <div>
+        <div className={styles.feature}>
           <h2>Brand Recognition</h2>
           <p>
             Boost your brand recognition with each link. Generic links don't
             mean a thing. Branded links help instill confidence in your content.
           </p>
         </div>
-        <div>
+        <div className={styles.feature}>
           <h2>Detailed Records</h2>
           <p>
             Gain insights into who is clicking your links. Knowing when and
             where people engage with your content helps inform better decisions.
           </p>
         </div>
-        <div>
+        <div className={styles.feature}>
           <h2>Fully Customizable</h2>
           <p>
             Improve brand awareness and content discoverability through

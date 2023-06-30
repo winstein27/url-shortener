3bf6e9762797386caa8e7ffb61fb62b15ade40d1
Winstein Martins <sriwinstein@gmail.com>
2023-06-30 15:45:53 -0300

    :sparkles: feat: features icons
    Adding icons to features section

--- src/components/main/Features.tsx
+++ src/components/main/Features.tsx
@@ -1,5 +1,9 @@
 import styles from "./Features.module.scss";
 
+import brandRecognition from "../../assets/images/icon-brand-recognition.svg";
+import detailedRecords from "../../assets/images/icon-detailed-records.svg";
+import fullyCustomizable from "../../assets/images/icon-fully-customizable.svg";
+
 const Features = () => {
   return (
     <div className={styles.features}>
@@ -12,6 +16,9 @@ const Features = () => {
       </div>
       <div>
         <div className={styles.feature}>
+          <div className={styles.image}>
+            <img src={brandRecognition} alt="" />
+          </div>
           <h2>Brand Recognition</h2>
           <p>
             Boost your brand recognition with each link. Generic links don't
@@ -19,6 +26,9 @@ const Features = () => {
           </p>
         </div>
         <div className={styles.feature}>
+          <div className={styles.image}>
+            <img src={detailedRecords} alt="" />
+          </div>
           <h2>Detailed Records</h2>
           <p>
             Gain insights into who is clicking your links. Knowing when and
@@ -26,6 +36,9 @@ const Features = () => {
           </p>
         </div>
         <div className={styles.feature}>
+          <div className={styles.image}>
+            <img src={fullyCustomizable} alt="" />
+          </div>
           <h2>Fully Customizable</h2>
           <p>
             Improve brand awareness and content discoverability through
